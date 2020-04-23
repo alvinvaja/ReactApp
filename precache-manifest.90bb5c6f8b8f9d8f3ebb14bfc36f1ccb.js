432f37cd3ced544a2228fa7af63eb6768742ab33
unknown <alvinvaja@gmail.com>
2020-04-23 14:36:10 +0700

    Updates

---
+++ precache-manifest.90bb5c6f8b8f9d8f3ebb14bfc36f1ccb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db94bda2d3cbb8298c35c5c1fde5925",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "b64254c6401fe86c62ba",
+    "url": "/ReactApp/static/css/main.bf9c8a42.chunk.css"
+  },
+  {
+    "revision": "92f97943d153fecf34d0",
+    "url": "/ReactApp/static/js/2.f9961933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.f9961933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64254c6401fe86c62ba",
+    "url": "/ReactApp/static/js/main.3d6778d8.chunk.js"
+  },
+  {
+    "revision": "50ef8a4e070e0012cedd",
+    "url": "/ReactApp/static/js/runtime-main.bf304976.js"
+  },
+  {
+    "revision": "6d53c5c56c449ebda1683d3c53995c3e",
+    "url": "/ReactApp/static/media/appsstore.6d53c5c5.svg"
+  },
+  {
+    "revision": "35a16fbfc9a8877f8db9008deec1b869",
+    "url": "/ReactApp/static/media/arrow-right.35a16fbf.svg"
+  },
+  {
+    "revision": "401da4cf448fcedab5d94c50c6ff28a7",
+    "url": "/ReactApp/static/media/ayam-koplo.401da4cf.svg"
+  },
+  {
+    "revision": "b30d9556fab712ebd23ecaad16049231",
+    "url": "/ReactApp/static/media/bude-sari.b30d9556.svg"
+  },
+  {
+    "revision": "7d58dd81f836b6fcd8df950e375dfb37",
+    "url": "/ReactApp/static/media/hangry.7d58dd81.svg"
+  },
+  {
+    "revision": "013cdb278d5ee132275691110e40beda",
+    "url": "/ReactApp/static/media/kopidaripada.013cdb27.svg"
+  },
+  {
+    "revision": "913c2cf6d9df09d8c06883724aa212e9",
+    "url": "/ReactApp/static/media/phone.913c2cf6.svg"
+  },
+  {
+    "revision": "9131bd104dc5d6fb93afa832714b887d",
+    "url": "/ReactApp/static/media/playstore.9131bd10.svg"
+  },
+  {
+    "revision": "807697ac38dba90ffc9ccb16d270ba52",
+    "url": "/ReactApp/static/media/san-gyu.807697ac.svg"
+  },
+  {
+    "revision": "687970d6ecb9a62bffbc8c096eeab363",
+    "url": "/ReactApp/static/media/sandwich.687970d6.svg"
+  }
+]);
